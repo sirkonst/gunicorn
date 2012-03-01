7dc4ef1dd0205efae471ea91508a8e78c2d48b4c
benoitc <bchesneau@gmail.com>
2012-03-01 15:47:41 +0100

    handle pep3333 encodings rules.
    Follow the spec. headers are transformed in nativesstring while the body
    is in bytes.
    
    hello world app is working.

--- gunicorn/http/wsgi.py
+++ gunicorn/http/wsgi.py
@@ -74,6 +74,7 @@ def create(req, sock, client, server, cfg):
     secure_headers = cfg.secure_scheme_headers
     x_forwarded_for_header = cfg.x_forwarded_for_header
 
+    print(req.headers)
     for hdr_name, hdr_value in req.headers:
         if hdr_name == "EXPECT":
             # handle expect
@@ -102,7 +103,7 @@ def create(req, sock, client, server, cfg):
 
     environ['wsgi.url_scheme'] = url_scheme
 
-    if isinstance(forward, basestring):
+    if isinstance(forward, py3compat.string_types):
         # we only took the last one
         # http://en.wikipedia.org/wiki/X-Forwarded-For
         if forward.find(",") >= 0:
@@ -129,7 +130,7 @@ def create(req, sock, client, server, cfg):
     environ['REMOTE_ADDR'] = remote[0]
     environ['REMOTE_PORT'] = str(remote[1])
 
-    if isinstance(server, basestring):
+    if isinstance(server, py3compat.string_types):
         server =  server.split(":")
         if len(server) == 1:
             if url_scheme == "http":
@@ -192,7 +193,8 @@ class Response(object):
 
     def process_headers(self, headers):
         for name, value in headers:
-            assert isinstance(name, basestring), "%r is not a string" % name
+            assert (isinstance(name, py3compat.string_types),
+                    "%r is not a string" % name)
             lname = name.lower().strip()
             if lname == "content-length":
                 self.response_length = int(value)
@@ -246,12 +248,13 @@ class Response(object):
             return
         tosend = self.default_headers()
         tosend.extend(["%s: %s\r\n" % (n, v) for n, v in self.headers])
-        util.write(self.sock, "%s\r\n" % "".join(tosend))
+        util.write(self.sock, py3compat.s2b("%s\r\n" % "".join(tosend)))
         self.headers_sent = True
 
     def write(self, arg):
         self.send_headers()
-        assert isinstance(arg, basestring), "%r is not a string." % arg
+        assert(isinstance(arg, py3compat.string_types),
+                "%r is not a string." % arg)
 
         arglen = len(arg)
         tosend = arglen
@@ -270,7 +273,7 @@ class Response(object):
             return
 
         self.sent += tosend
-        util.write(self.sock, arg, self.chunked)
+        util.write(self.sock, py3compat.s2b(arg), self.chunked)
 
     def sendfile_all(self, fileno, sockno, offset, nbytes):
         # Send file in at most 1GB blocks as some operating
@@ -309,12 +312,12 @@ class Response(object):
             self.send_headers()
 
             if self.is_chunked():
-                self.sock.sendall("%X\r\n" % nbytes)
+                self.sock.sendall(py3compat.s2b("%X\r\n" % nbytes))
 
             self.sendfile_all(fileno, self.sock.fileno(), fo_offset, nbytes)
 
             if self.is_chunked():
-                self.sock.sendall("\r\n")
+                self.sock.sendall(b"\r\n")
 
             os.lseek(fileno, fd_offset, os.SEEK_SET)
         else:
@@ -325,4 +328,4 @@ class Response(object):
         if not self.headers_sent:
             self.send_headers()
         if self.chunked:
-            util.write_chunk(self.sock, "")
+            util.write_chunk(self.sock, b"")
